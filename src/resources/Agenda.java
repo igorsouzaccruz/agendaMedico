cf54cbbe27a4c337fdf6c8915d4c0cdfd101afbb
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-11 16:04:23 -0300

    add metodo confirmar

--- src/resources/Agenda.java
+++ src/resources/Agenda.java
@@ -7,28 +7,40 @@ import entities.Consulta;
 import entities.exceptions.ConsultaException;
 
 public class Agenda {
-    
+
     List<Consulta> consultas = new ArrayList<Consulta>();
 
-    public void adicionar(Consulta consulta){    
-
-        if(disponibilidade(consulta)){ 
+    public void adicionar(Consulta consulta) {
+            confirmar(consulta);
             this.consultas.add(consulta);
             System.out.println("Add success");
+    }
+
+    private void confirmar(Consulta consulta) {
+        if (disponibilidade(consulta)) {
+            if (consulta.getStatus() == 0) {
+                consulta.setStatus(1);
+                System.out.println("Confirmado");
+            } else if (consulta.getStatus() == 1) {
+                throw new ConsultaException("Status Inválido");
+            } else {
+                throw new ConsultaException("Consulta não encontrada");
+            }
         } else {
             throw new ConsultaException("Horário Ocupado!");
         }
     }
 
-    private boolean disponibilidade(Consulta consulta){
+    private boolean disponibilidade(Consulta consulta) {
         boolean disponibilidade = true;
 
-        for(Consulta consult: this.consultas) { 
+        for (Consulta consult : this.consultas) {
 
-            if(consult.equals(consulta)){
+            if (consult.equals(consulta)) {
                 disponibilidade = false;
             }
-        }; 
-        return disponibilidade;        
+        }
+        
+        return disponibilidade;
     }
 }

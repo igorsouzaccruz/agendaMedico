6e760f27539d7a71d04cb2982a866ec09428f0de
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2022-12-11 21:39:53 -0300

    refatorando

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ public class App {
         try {
             Consulta consuta01 = new Consulta(0, 0, 0, 0, null);
             Consulta consuta02 = new Consulta(0, 0, 0, 0, null);
-            Consulta consuta03 = new Consulta(0, 0, 0, 0, null);
+            // Consulta consuta03 = new Consulta(0, 0, 0, 0, null);
           
             Agenda agenda = new Agenda();
 
@@ -19,15 +19,10 @@ public class App {
 
             agenda.adicionar(consuta02);
             System.out.println(consuta02.getStatus());
-
+            
             agenda.realizar(consuta02);
             System.out.println(consuta02.getStatus());
 
-            agenda.adicionar(consuta03);
-            System.out.println(consuta03.getStatus());
-
-            agenda.realizar(consuta03);
-            System.out.println(consuta03.getStatus());
 
         } catch (Exception e) {
             System.out.println(e);
